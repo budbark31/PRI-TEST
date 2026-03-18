34b1f90c7c850cf63e54d03041a77eb7a1e8e365
Budbark31 <bivinsshilo@gmail.com>
2026-03-18 16:25:30 -0400

    fix: align Stripe API version with SDK types

--- app/actions/stripeCheckout.ts
+++ app/actions/stripeCheckout.ts
@@ -9,7 +9,7 @@ if (!stripeSecretKey) {
 }
 
 const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2026-02-25.clover",
 });
 
 export async function createCheckoutSession(part: {
